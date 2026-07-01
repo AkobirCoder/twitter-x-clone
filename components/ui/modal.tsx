909364080ba92507bca8be10d9abaf4e32ba7f54
AkobirCoder <akobir0308@gmail.com>
2026-07-01 23:52:17 +0500

    components/ui/modal.tsx was updated

--- components/ui/modal.tsx
+++ components/ui/modal.tsx
@@ -1,31 +1,37 @@
 import React, { ReactElement } from 'react';
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from '../ui/dialog';
+import { Dialog, DialogContent } from '../ui/dialog';
+import { X } from 'lucide-react';
 
 interface ModalProps {
-    title?: string,
-    body?: ReactElement,
-    footer?: ReactElement,
     step?: number,
     totalSteps?: number,
+    body?: ReactElement,
+    footer?: ReactElement,
     isOpen?: boolean,
     onClose?: () => void,
 }
 
 export default function Modal({
-    title,
-    body,
-    footer,
     step,
     totalSteps,
+    body,
+    footer,
     isOpen,
     onClose
 }: ModalProps) {
     return (
         <Dialog open={isOpen} onOpenChange={onClose}>
-            <DialogContent className='bg-black'>
-                <DialogHeader>
-                    <DialogTitle>{title}</DialogTitle>
-                </DialogHeader>
+            <DialogContent className='bg-black p-1'>
+                <div className='flex items-center gap-4'>
+                    <button className='p-1 border-0 text-white hover:opacity-70 transition w-fit cursor-pointer'>
+                        <X size={28} onClick={onClose} />
+                    </button>
+                    {
+                        step && totalSteps && (
+                            <div className='text-xl font-bold'>Step {step} of {totalSteps}</div>
+                        )
+                    }
+                </div>
                 <div className='mt-4'>{body}</div>
                 {
                     footer && <div className='mt-4'>{footer}</div>
