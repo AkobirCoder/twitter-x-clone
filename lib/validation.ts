4c752ff9d7dd0466082b64430332b8b559e91b91
AkobirCoder <akobir0308@gmail.com>
2026-07-03 00:18:08 +0500

    lib/validation.ts was updated

--- lib/validation.ts
+++ lib/validation.ts
@@ -9,3 +9,8 @@ export const registerStep2Schema = z.object({
     username: z.string().min(3, 'Username must be at least 3 characters long.'),
     password: z.string().min(6, 'Password must be at least 6 characters long.'),
 });
+
+export const loginSchema = z.object({
+    email: z.string().email('Please enter a valid email address.'),
+    password: z.string().min(6, 'Password must be at least 6 characters long.'),
+});
