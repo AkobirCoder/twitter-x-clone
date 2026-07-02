bc610a332190fd7f17db46f1a7d44d1dccb09a6a
AkobirCoder <akobir0308@gmail.com>
2026-07-02 22:31:07 +0500

    lib/validation.ts was updated

--- lib/validation.ts
+++ lib/validation.ts
@@ -1,11 +1,11 @@
 import * as z from 'zod';
 
 export const registerStep1Schema = z.object({
-    email: z.string().email(),
-    name: z.string().min(3),
+    name: z.string().min(3, 'Name must be at least 3 characters long.'),
+    email: z.string().email('Please enter a valid email address.'),
 });
 
 export const registerStep2Schema = z.object({
-    password: z.string().min(6),
-    username: z.string().min(3),
+    username: z.string().min(3, 'Username must be at least 3 characters long.'),
+    password: z.string().min(6, 'Password must be at least 6 characters long.'),
 });
