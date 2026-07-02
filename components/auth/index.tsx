9f0901f7f5e326e9e74cebcf5b3a32235f423393
AkobirCoder <akobir0308@gmail.com>
2026-07-03 00:16:24 +0500

    components/auth/index.tsx was updated

--- components/auth/index.tsx
+++ components/auth/index.tsx
@@ -5,19 +5,28 @@ import React, { useCallback } from 'react';
 import Button from '../ui/button';
 import { FcGoogle } from 'react-icons/fc';
 import { AiFillGithub } from 'react-icons/ai';
-import { useRegisterModal } from '@/hooks/use-register-modal';
+import useRegisterModal from '@/hooks/use-register-modal';
 import RegisterModal from '../modals/register-modal';
+import useLoginModal from '@/hooks/use-login-modal';
+import LoginModal from '../modals/login-modal';
 
 export default function Auth() {
     const registerModal = useRegisterModal();
+
+    const loginModal = useLoginModal();
 
     const onOpenRegisterModal = useCallback(() => {
         registerModal.onOpen();
     }, [registerModal]);
 
+    const onOpenLoginModal = useCallback(() => {
+        loginModal.onOpen();
+    }, [loginModal]);
+
     return (
         <>
             <RegisterModal />
+            <LoginModal />
             <div className='grid grid-cols-1 md:grid-cols-2 gap-10 items-center h-screen p-5 md:p-0'>
                 <Image 
                     src={'/images/x.svg'} 
@@ -70,9 +79,9 @@ export default function Auth() {
                                 onClick={onOpenRegisterModal}
                             />
                             <div className='text-[10px] text-gray-400'>
-                                By signing up, you agree to the <span className='text-sky-500'>Terms of Service</span> and
-                                <span className='text-sky-500'> Privacy Policy</span>, including
-                                <span className='text-sky-500'> Cookie Use</span>.
+                                By signing up, you agree to the <span className='text-sky-500 cursor-pointer'>Terms of Service</span> and
+                                <span className='text-sky-500 cursor-pointer'> Privacy Policy</span>, including
+                                <span className='text-sky-500 cursor-pointer'> Cookie Use</span>.
                             </div>
                         </div>
                     </div>
@@ -84,6 +93,7 @@ export default function Auth() {
                             label={'Sign in'}
                             fullWidth
                             outline
+                            onClick={onOpenLoginModal}
                         />
                     </div>
                 </div>
