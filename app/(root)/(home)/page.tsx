6487e6a6f3b4bab063d3c4272737590753a7cbcb
AkobirCoder <akobir0308@gmail.com>
2026-07-06 11:30:11 +0500

    app/(root)/(home)/page.tsx was updated

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,16 +1,16 @@
-import Auth from '@/components/auth';
+// import Auth from '@/components/auth';
 import React from 'react';
 
 export default function Page() {
-    const user = false;
+    // const user = false;
 
-    if (!user) {
-        return (
-            <div className='container h-screen max-w-7xl mx-auto'>
-                <Auth />
-            </div>
-        );
-    }
+    // if (!user) {
+    //     return (
+    //         <div className='container h-screen max-w-7xl mx-auto'>
+    //             <Auth />
+    //         </div>
+    //     );
+    // }
 
     return (
         <div>Page</div>
