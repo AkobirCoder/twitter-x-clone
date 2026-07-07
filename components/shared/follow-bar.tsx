79ff21f8862be8023476e8ef09eb28ab6d7bf271
AkobirCoder <akobir0308@gmail.com>
2026-07-08 01:34:45 +0500

    components/shared/follow-bar.tsx was updated

--- components/shared/follow-bar.tsx
+++ components/shared/follow-bar.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
+import Button from '../ui/button';
+import { Loader2 } from 'lucide-react';
+import User from './user';
 
 const FollowBar = () => {
+    const isLoaded = false;
+
     return (
-        <div>FollowBar</div>
+        <div className='py-4 hidden lg:block w-66.5'>
+            <div className='bg-neutral-800 rounded-md px-4 py-2'>
+                <div className='flex items-center justify-between pb-2 border-b-2'>
+                    <h2 className='text-white/80'>Who to follow</h2>
+                    <Button
+                        label={'See all'}
+                        secondary
+                        className='rounded-md px-4 py-1 font-light'
+                    />
+                </div>
+
+                {
+                    isLoaded ? (
+                        <div className='flex items-center justify-center h-24'>
+                            <Loader2 className='animate-spin text-sky-500' />
+                        </div>
+                    ): (
+                        <div className='flex flex-col gap-6 mt-2'>
+                            <User />
+                        </div>
+                    )
+                }
+                
+            </div>
+        </div>
     );
 }
 
