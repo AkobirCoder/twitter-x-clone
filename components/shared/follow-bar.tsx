b97db09ca5957262fdfae50ae84f222023ee6a69
AkobirCoder <akobir0308@gmail.com>
2026-07-09 01:20:27 +0500

    components/shared/follow-bar.tsx was updated

--- components/shared/follow-bar.tsx
+++ components/shared/follow-bar.tsx
@@ -1,31 +1,46 @@
+"use client"
+
 import React from 'react';
 import Button from '../ui/button';
 import { Loader2 } from 'lucide-react';
 import User from './user';
+import useUsers from '@/hooks/use-users';
+import { IUser } from '@/types';
+import Link from 'next/link';
 
 const FollowBar = () => {
-    const isLoaded = false;
+    const { isLoading, users } = useUsers(10);
+
+    console.log(users);
 
     return (
-        <div className='py-4 hidden lg:block w-66.5'>
+        <div className='sticky top-0 right-0 h-screen py-4 hidden lg:block w-66.5'>
             <div className='bg-neutral-800 rounded-md px-4 py-2'>
                 <div className='flex items-center justify-between pb-2 border-b-2'>
                     <h2 className='text-white/80'>Who to follow</h2>
                     <Button
                         label={'See all'}
                         secondary
-                        className='rounded-md px-4 py-1 font-light'
+                        className='rounded-md px-4 py-1 font-light text-sm'
                     />
                 </div>
 
                 {
-                    isLoaded ? (
+                    isLoading ? (
                         <div className='flex items-center justify-center h-24'>
                             <Loader2 className='animate-spin text-sky-500' />
                         </div>
                     ): (
-                        <div className='flex flex-col gap-6 mt-2'>
-                            <User />
+                        <div className='flex flex-col gap-1 mt-2'>
+                            {
+                                users && users.map((user: IUser) => {
+                                    return (
+                                        <Link key={user._id} href={`/profile/${user._id}`}>
+                                            <User user={user} />
+                                        </Link>
+                                    );
+                                })
+                            }
                         </div>
                     )
                 }
